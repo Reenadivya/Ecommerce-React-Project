0e47122393bbbf16c2f7106cde995f2f97f2e888
Reenadivya <reenad286@gmail.com>
2022-07-17 23:19:24 +0800

    init commit

---
+++ src/components/category-item/directory/directory.component.jsx
@@ -0,0 +1,13 @@
+import CatergoryItem from "../category-item.component";
+import "./directory.styles.scss";
+const Directory = ({ categories }) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CatergoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
