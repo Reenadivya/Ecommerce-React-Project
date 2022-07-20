0ea9b227985f9306f00026750949ff0b354a6022
Reenadivya <reenad286@gmail.com>
2022-07-21 01:05:53 +0800

    sign up form updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/home/navigation/navigation.component";
 import Home from "./routes/home/home.component";
+import SignIn from "./routes/sign-in/sign-in.components";
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -12,6 +13,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="signIn" element={<SignIn />} />
       </Route>
     </Routes>
   );
