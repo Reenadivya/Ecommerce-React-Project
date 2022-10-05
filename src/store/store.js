fe155149e13067ff8b1564471b36b4389b0bad05
Reenadivya <reenad286@gmail.com>
2022-10-05 16:40:14 +0800

    migrated from context to redux

---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+import { compose, createStore, applyMiddleware } from "redux";
+// import logger from "redux-logger";
+
+import { rootReducer } from "./root-reducer";
+
+const loggerMiddleware = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+
+  next(action);
+
+  console.log("next state", store.getState());
+};
+
+const middleWares = [loggerMiddleware];
+
+const composedEnhancers = compose(applyMiddleware(...middleWares));
+
+export const store = createStore(rootReducer, undefined, composedEnhancers);
